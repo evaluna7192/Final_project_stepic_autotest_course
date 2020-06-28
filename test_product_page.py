7893c5c306844cae428584a50cbf671ed37857f1
Дарья Гимпель <gimpel@devel.ifx>
2020-06-28 20:02:45 +0300

    delete unnecessary code

--- test_product_page.py
+++ test_product_page.py
@@ -18,8 +18,6 @@ import pytest
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
 def test_guest_can_add_product_to_basket(link, browser):
-    #link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-    #link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
     product_page = ProductPage(browser,link)
     product_page.open()
     product_page.add_product_to_basket()
